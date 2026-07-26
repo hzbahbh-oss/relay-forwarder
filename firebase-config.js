196cafdb9f1673d41aded525dbc305a12c984877
hzbahbh-oss <hzbahbh@gmail.com>
2026-07-27 00:59:20 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase config for project: relay-forwarder
+window.firebaseConfig = {
+  apiKey: "AIzaSyBDzSBwI1tKW3m_qg1GGO-2CpWUrSybmIs",
+  authDomain: "relay-forwarder.firebaseapp.com",
+  projectId: "relay-forwarder",
+  storageBucket: "relay-forwarder.firebasestorage.app",
+  messagingSenderId: "162595830065",
+  appId: "1:162595830065:web:81fd2dcbe36a5707e81810"
+};
